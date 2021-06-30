19558f2139d85a897fc21e90f38e726226b9685c
altair-coin <altair-coin@yandex.ru>
2021-06-30 17:58:49 +0200

    Initial commit

---
+++ src/P2p/P2pNetworks.h
@@ -0,0 +1,10 @@
+// Copyright (c) 2011-2016 The Cryptonote developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#pragma once
+
+namespace CryptoNote
+{
+  const static boost::uuids::uuid CRYPTONOTE_NETWORK = { { 0x4, 0x52, 0x5, 0x50, 0x54, 0x4f, 0x4e, 0x4f, 0x4, 0x45, 0x00, 0x2, 0x10, 0x11, 0x01, 0x10 } };
+}
